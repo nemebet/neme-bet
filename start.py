01c3713907b6a904384059c9673135dc258f9909
swatnemesiz <swatfest2026@gmail.com>
2026-04-04 10:30:50 -0600

    Fix start.py - use webapp not webapp_minimal

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
 import os
 import subprocess
 port = os.environ.get('PORT', '5000')
-subprocess.run(['gunicorn', 'webapp_minimal:app', '--bind', f'0.0.0.0:{port}', '--workers', '1'])
+subprocess.run(['gunicorn', 'webapp:app', '--bind', f'0.0.0.0:{port}', '--workers', '1', '--timeout', '120'])
