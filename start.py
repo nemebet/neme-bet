a7b7429146bd2175a6d7425bd07b2a6f2212a140
swatnemesiz <swatfest2026@gmail.com>
2026-04-09 11:13:33 -0600

    Fix env vars inheritance in start.py

--- start.py
+++ start.py
@@ -1,4 +1,10 @@
 import os
 import subprocess
+
 port = os.environ.get('PORT', '5000')
-subprocess.run(['gunicorn', 'webapp:app', '--bind', f'0.0.0.0:{port}', '--workers', '1', '--timeout', '120'])
+
+# Pasar todas las variables de entorno al proceso gunicorn
+subprocess.run(
+    ['gunicorn', 'webapp:app', '--bind', f'0.0.0.0:{port}', '--workers', '1', '--timeout', '120'],
+    env=os.environ.copy()
+)
